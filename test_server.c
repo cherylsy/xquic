86f2d12045b877da1bacb9ff5863de4fad9ae627
miaoji.lym <miaoji.lym@alibaba-inc.com>
2019-03-06 21:19:49 +0800

    add CMakeLists.txt

---
+++ test_server.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "xqc_cmake_config.h"
+ 
+int main (int argc, char *argv[])
+{
+    printf("Usage: %s Version %d.%d\n", argv[0], xquic_VERSION_MAJOR, xquic_VERSION_MINOR);
+
+
+    return 0;
+}
