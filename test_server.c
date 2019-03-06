78885c3f09f66b83afe48c88d3e00393ae7acdfb
miaoji.lym <miaoji.lym@alibaba-inc.com>
2019-03-06 22:19:15 +0800

    add_library in CMakeLists.txt

--- test_server.c
+++ test_server.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include <math.h>
 #include "xqc_cmake_config.h"
- 
+#include "transport/xqc_conn.h"
+
 int main (int argc, char *argv[])
 {
-    printf("Usage: %s Version %d.%d\n", argv[0], xquic_VERSION_MAJOR, xquic_VERSION_MINOR);
-
+    printf("Usage: %s XQUIC:%d.%d Transport:%s\n", argv[0], xquic_VERSION_MAJOR, xquic_VERSION_MINOR, XQC_TRANSPORT_VERSION);
 
     return 0;
 }
