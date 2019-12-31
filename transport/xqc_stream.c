cccb8c76296bafe690ae5d55aa6072f8a7500dd3
以天 <jiuhai.zjh@alibaba-inc.com>
2019-12-31 16:07:14 +0800

    EAGAIN先返回0，等待测试完成

--- transport/xqc_stream.c
+++ transport/xqc_stream.c
@@ -1074,7 +1074,7 @@ do_buff:
 
     if (offset == 0 && !fin_only_done) {
         if (ret == -XQC_EAGAIN) {
-            return -XQC_EAGAIN; // -XQC_EAGAIN not means error
+            return 0; // -XQC_EAGAIN not means error
         } else {
             XQC_CONN_ERR(conn, TRA_INTERNAL_ERROR);
             return ret;
@@ -1176,12 +1176,10 @@ xqc_process_write_streams (xqc_connection_t *conn)
         xqc_log(conn->log, XQC_LOG_DEBUG, "|stream_write_notify|flag:%d|stream_id:%ui|",
                 stream->stream_flag, stream->stream_id);
         ret = stream->stream_if->stream_write_notify(stream, stream->user_data);
-        if (ret < 0) {
+        if (ret < 0 && ret != -XQC_EAGAIN) {
             xqc_log(conn->log, XQC_LOG_ERROR, "|stream_write_notify err:%d|flag:%d|stream_id:%ui|",
                     ret, stream->stream_flag, stream->stream_id);
-            if (ret != -XQC_EAGAIN) {
-                xqc_stream_shutdown_write(stream);
-            }
+            xqc_stream_shutdown_write(stream);
             if (ret != -XQC_ESTREAM_RESET && ret != -XQC_CLOSING && ret != -XQC_EAGAIN) {
                 XQC_CONN_ERR(conn, TRA_INTERNAL_ERROR);
             }
@@ -1202,12 +1200,10 @@ xqc_process_read_streams (xqc_connection_t *conn)
         xqc_log(conn->log, XQC_LOG_DEBUG, "|stream_read_notify|flag:%d|stream_id:%ui|",
                 stream->stream_flag, stream->stream_id);
         ret = stream->stream_if->stream_read_notify(stream, stream->user_data);
-        if (ret < 0) {
+        if (ret < 0 && ret != -XQC_EAGAIN) {
             xqc_log(conn->log, XQC_LOG_ERROR, "|stream_read_notify err:%d|flag:%d|stream_id:%ui|",
                     ret, stream->stream_flag, stream->stream_id);
-            if (ret != -XQC_EAGAIN) {
-                xqc_stream_shutdown_write(stream);
-            }
+            xqc_stream_shutdown_write(stream);
             if (ret != -XQC_ESTREAM_RESET && ret != -XQC_CLOSING && ret != -XQC_EAGAIN) {
                 XQC_CONN_ERR(conn, TRA_INTERNAL_ERROR);
             }
