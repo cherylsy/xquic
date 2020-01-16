5e1f42c510634dfdcb6572fa6241061bf98b75e7
dreamwind.ll <dreamwind.ll@alibaba-inc.com>
2020-01-16 16:04:24 +0800

    fix

--- qpack_test.sh
+++ qpack_test.sh
@@ -22,11 +22,14 @@ grep_qpack_test_result() {
 }
 
 clear_log
-echo "测试qpack never flag:"
-echo "测试qpack 哈希表insert:"
-echo "测试qpack literial 编码:"
-echo "测试qpack name value index:"
-echo "测试qpack name index:"
+echo "[1]测试qpack insert literial:"
+echo "[2]测试qpack never flag:"
+echo "[3]测试qpack literial 模式发送:"
+echo "[4]测试qpack 静态表 static name value index 编码:"
+echo "[5]测试qpack 静态表 static name index 编码:"
+echo "[6]测试qpack 动态表 name value index 编码:"
+echo "[7]测试qpack 动态表 name index 编码:"
+echo "[8]测试qpack 动态表 draining:"
 ./test_qpack_client | grep "qpack test" 
 grep_qpack_test_result
 grep_err_log|grep -v xqc_conn_check_token
