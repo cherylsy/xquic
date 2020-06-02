56709bfc82d139fbee5c74c212d5493fd0f3cb72
以天 <jiuhai.zjh@alibaba-inc.com>
2020-06-02 21:23:34 +0800

    add qpack test

--- build/qpack_test.sh
+++ build/qpack_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 另一个终端启动server
-#killall test_qpack_server 2> /dev/null
-#./test_qpack_server  > /dev/null &
+killall test_qpack_server 2> /dev/null
+./test_qpack_server  > /dev/null &
 sleep 1
 
 clear_log() {
@@ -13,8 +13,8 @@ clear_log() {
 grep_err_log() {
     grep "\[error\]" clog
     grep "\[error\]" slog
-    grep "retrans rate:" clog|grep -v "retrans rate:0.0000"
-    grep "retrans rate:" slog|grep -v "retrans rate:0.0000"
+    #grep "retrans rate:" clog|grep -v "retrans rate:0.0000"
+    #grep "retrans rate:" slog|grep -v "retrans rate:0.0000"
 }
 
 grep_qpack_test_result() {
@@ -22,15 +22,16 @@ grep_qpack_test_result() {
 }
 
 clear_log
-echo "[1]测试qpack insert literial:"
-echo "[2]测试qpack never flag:"
-echo "[3]测试qpack literial 模式发送:"
-echo "[4]测试qpack 静态表 static name value index 编码:"
-echo "[5]测试qpack 静态表 static name index 编码:"
-echo "[6]测试qpack 动态表 name value index 编码:"
-echo "[7]测试qpack 动态表 name index 编码:"
-echo "[8]测试qpack 动态表 draining:"
+#echo "[1]测试qpack insert literial:"
+#echo "[2]测试qpack never flag:"
+#echo "[3]测试qpack literial 模式发送:"
+#echo "[4]测试qpack 静态表 static name value index 编码:"
+#echo "[5]测试qpack 静态表 static name index 编码:"
+#echo "[6]测试qpack 动态表 name value index 编码:"
+#echo "[7]测试qpack 动态表 name index 编码:"
+#echo "[8]测试qpack 动态表 draining:"
 ./test_qpack_client | grep "qpack test" 
 grep_qpack_test_result
 grep_err_log|grep -v xqc_conn_check_token
 
+killall test_qpack_server
