45c094e66a9d41016b1af58655800ee436b48fce
静笃 <cwt274890@alibaba-inc.com>
2020-12-22 14:13:56 +0800

    [!]修改xqc_process_frame_test中错误帧设置错误的bug；增加new_token帧token为空的单测

--- tests/xqc_process_frame_test.c
+++ tests/xqc_process_frame_test.c
@@ -3,8 +3,10 @@
 #include "xqc_common_test.h"
 #include "src/transport/xqc_frame.h"
 #include "src/transport/xqc_packet_in.h"
+#include "src/common/xqc_variable_len_int.h"
 
-#define ILL_FRAME_1 "/xff/x00/x00/x00"
+char ILL_FRAME_1[] = {0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+char ZERO_LEN_NEW_TOKEN_FRAME[] = {0x07, 0x00};
 void xqc_test_process_frame()
 {
     xqc_connection_t *conn = test_engine_connect();
@@ -12,7 +14,12 @@ void xqc_test_process_frame()
 
     xqc_packet_in_t packet_in;
     packet_in.pos = ILL_FRAME_1;
-    packet_in.last = packet_in.pos + sizeof(ILL_FRAME_1) - 1;
+    packet_in.last = packet_in.pos + sizeof(ILL_FRAME_1);
     int ret = xqc_process_frames(conn, &packet_in);
     CU_ASSERT(ret == -XQC_EILLPKT);
+
+    packet_in.pos = ZERO_LEN_NEW_TOKEN_FRAME;
+    packet_in.last = packet_in.pos + sizeof(ZERO_LEN_NEW_TOKEN_FRAME);
+    ret = xqc_process_frames(conn, &packet_in);
+    CU_ASSERT(ret == -XQC_EPROTO);
 }
