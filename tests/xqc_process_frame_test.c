f39448f425b14ae76d78a430cac11771a2519b65
zhaowu <zhaowu.zw@alibaba-inc.com>
2020-12-23 17:33:53 +0800

    add padding frame test case

--- tests/xqc_process_frame_test.c
+++ tests/xqc_process_frame_test.c
@@ -8,6 +8,8 @@
 char XQC_TEST_ILL_FRAME_1[] = {0xff, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
 char XQC_TEST_ZERO_LEN_NEW_TOKEN_FRAME[] = {0x07, 0x00};
 char XQC_TEST_OVERAGE_STREAM_BLOCKED_FRAME[] = {0x16, 0xD0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01};
+
+
 void xqc_test_process_frame()
 {
     xqc_connection_t *conn = test_engine_connect();
@@ -28,4 +30,30 @@ void xqc_test_process_frame()
     packet_in.last = packet_in.pos + sizeof(XQC_TEST_OVERAGE_STREAM_BLOCKED_FRAME);
     ret = xqc_process_frames(conn, &packet_in);
     CU_ASSERT(ret == -XQC_EPARAM);
+}
+
+
+void xqc_test_parse_padding_frame()
+{
+    xqc_connection_t *conn = test_engine_connect();
+    CU_ASSERT(conn != NULL);
+
+    char XQC_PURE_PADDING_FRAME[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
+    xqc_packet_in_t pi_padding;
+    memset(&pi_padding, 0, sizeof(xqc_packet_in_t));
+    pi_padding.pos = XQC_PURE_PADDING_FRAME;
+    pi_padding.last = pi_padding.pos + sizeof(XQC_PURE_PADDING_FRAME);
+    int ret = xqc_process_frames(conn, &pi_padding);
+    CU_ASSERT(ret == XQC_OK);
+    CU_ASSERT(pi_padding.pi_frame_types == XQC_FRAME_BIT_PADDING);
+
+    /* MAX_DATA frame after PADDING frame */
+    char XQC_MIXED_PADDING_FRAME[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x3F};
+    xqc_packet_in_t pi_padding_mix;
+    memset(&pi_padding_mix, 0, sizeof(xqc_packet_in_t));
+    pi_padding_mix.pos = XQC_MIXED_PADDING_FRAME;
+    pi_padding_mix.last = pi_padding_mix.pos + sizeof(XQC_MIXED_PADDING_FRAME);
+    ret = xqc_process_frames(conn, &pi_padding_mix);
+    CU_ASSERT(ret == XQC_OK);
+    CU_ASSERT(pi_padding_mix.pi_frame_types == (XQC_FRAME_BIT_PADDING | XQC_FRAME_BIT_MAX_DATA));
 }
