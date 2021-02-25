d543fa4ae1f9c52ab9dfdb13f62ba798bf41d898
zhaowu <zhaowu.zw@alibaba-inc.com>
2021-02-25 21:21:03 +0800

    [!] 1) insert server's conn object into dcid hash after successful decryption of Initial packet; 2) insert conn object into engine's conns_hash is more related to outer caller

--- src/transport/xqc_conn.c
+++ src/transport/xqc_conn.c
@@ -255,19 +255,6 @@ xqc_conn_create(xqc_engine_t *engine,
         goto fail;
     }
 
-    /* Insert into engine's conns_hash */
-    if (xqc_insert_conns_hash(engine->conns_hash, xc, &xc->scid)) {
-        goto fail;
-    }
-
-    /* 客户端创建连接时，服务端的cid还未生成 */
-    if (type == XQC_CONN_TYPE_SERVER) {
-        if (xqc_insert_conns_hash(engine->conns_hash_dcid, xc, &xc->dcid)) {
-            goto fail;
-        }
-        xc->conn_flag |= XQC_CONN_FLAG_DCID_OK;
-    }
-
     for (xqc_pkt_num_space_t i = 0; i < XQC_PNS_N; i++) {
         memset(&xc->recv_record[i], 0, sizeof(xqc_recv_record_t));
         xqc_init_list_head(&xc->recv_record[i].list_head);
@@ -329,6 +316,11 @@ xqc_conn_server_create(xqc_engine_t *engine,
     if (conn == NULL) {
         xqc_log(engine->log, XQC_LOG_ERROR, "|fail to create connection|");
         return NULL;
+    }
+
+    /* Insert into engine's conns_hash */
+    if (xqc_insert_conns_hash(engine->conns_hash, xc, &xc->scid)) {
+        goto fail;
     }
 
     xqc_cid_copy(&conn->ocid, scid);
