f54233dcbe2f2cf60562118b60a47baa4dac423b
zhaowu <zhaowu.zw@alibaba-inc.com>
2021-04-14 16:08:22 +0800

    修复bool类型定义问题

--- src/transport/xqc_send_ctl.c
+++ src/transport/xqc_send_ctl.c
@@ -1675,7 +1675,7 @@ xqc_send_ctl_get_retrans_rate(xqc_send_ctl_t *ctl)
 xqc_bool_t
 xqc_send_ctl_check_anti_amplification(xqc_connection_t *conn, int byte_cnt)
 {
-    int limit = XQC_FALSE;
+    xqc_bool_t limit = XQC_FALSE;
     /* anti-amplifier attack limit */
     if (conn->conn_type == XQC_CONN_TYPE_SERVER
         && !(conn->conn_flag & XQC_CONN_FLAG_ADDR_VALIDATED))
