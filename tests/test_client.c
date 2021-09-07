10a5866510123bef69211f3901cc5b6b142ef730
zhaowu <zhaowu.zw@alibaba-inc.com>
2021-09-07 19:54:16 +0800

    [!] fix test case error

--- tests/test_client.c
+++ tests/test_client.c
@@ -292,9 +292,10 @@ xqc_client_write_socket(
     memcpy(send_buf, buf, send_buf_size);
 
     /* trigger version negotiation */
-    if (g_test_case == 31) {
+    if (g_test_case == 33) {
         /* makes version 0xff000001 */
         send_buf[1] = 0xff;
+        g_test_case == -1;
     }
 
     //printf("xqc_client_write_socket size=%zd, now=%llu, send_total=%d\n",size, now(), ++g_send_total);
