0604bed615cd26406274286b3bf811f34b9c4ded
zhaowu <zhaowu.zw@alibaba-inc.com>
2021-09-09 12:53:53 +0800

    [!] 回退反放大限制使用编码后长度，当前状态并不统一，后续再改进

--- src/transport/xqc_send_ctl.c
+++ src/transport/xqc_send_ctl.c
@@ -827,8 +827,7 @@ xqc_send_ctl_update_stream_stats_on_sent(xqc_send_ctl_t *ctl,
  * OnPacketSent
  */
 void
-xqc_send_ctl_on_packet_sent(xqc_send_ctl_t *ctl, xqc_packet_out_t *packet_out, xqc_usec_t now,
-    size_t sent)
+xqc_send_ctl_on_packet_sent(xqc_send_ctl_t *ctl, xqc_packet_out_t *packet_out, xqc_usec_t now)
 {
     xqc_pkt_num_space_t pns = packet_out->po_pkt.pkt_pns;
 
@@ -847,7 +846,7 @@ xqc_send_ctl_on_packet_sent(xqc_send_ctl_t *ctl, xqc_packet_out_t *packet_out, x
         ctl->ctl_largest_sent[pns] = packet_out->po_pkt.pkt_num;
     }
 
-    ctl->ctl_bytes_send += sent;
+    ctl->ctl_bytes_send += packet_out->po_used_size;
 
     if (XQC_CAN_IN_FLIGHT(packet_out->po_frame_types)) {
 
