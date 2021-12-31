2aa69450df695146d05a191b48de8920370183db
zhaowu <zhaowu.zw@alibaba-inc.com>
2021-12-31 22:39:30 +0800

    fix compile error

--- src/transport/xqc_client.c
+++ src/transport/xqc_client.c
@@ -151,7 +151,7 @@ xqc_client_create_tls(xqc_connection_t *conn, const xqc_conn_ssl_config_t *conn_
         ret = -XQC_EMALLOC;
         goto end;
     }
-    strncpy(cfg.alpn, alpn_cap, alpn);
+    strncpy(cfg.alpn, alpn, alpn_cap);
 
     /* copy hostname */
     host_cap = strlen(hostname) + 1;
@@ -161,7 +161,7 @@ xqc_client_create_tls(xqc_connection_t *conn, const xqc_conn_ssl_config_t *conn_
         ret = -XQC_EMALLOC;
         goto end;
     }
-    strncpy(cfg.hostname, host_cap, hostname);
+    strncpy(cfg.hostname, hostname, host_cap);
 
     /* encode local transport parameters, and set to tls config */
     cfg.trans_params = tp_buf;
