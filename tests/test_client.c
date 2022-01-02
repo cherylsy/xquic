e5cfb488f1d51921e4cb8e7a2dfbe67b7dffb461
zhaowu <zhaowu.zw@alibaba-inc.com>
2022-01-02 17:21:19 +0800

    fix BUFFER_SIZE

--- tests/test_client.c
+++ tests/test_client.c
@@ -514,7 +514,7 @@ xqc_client_bind_to_interface(int fd,
 {
     struct ifreq ifr;
     memset(&ifr, 0x00, sizeof(ifr));
-    strncpy(ifr.ifr_name, interface_name, sizeof(ifr.ifr_name));
+    strncpy(ifr.ifr_name, interface_name, sizeof(ifr.ifr_name) - 1);
 
     printf("bind to nic: %s\n", interface_name);
 
